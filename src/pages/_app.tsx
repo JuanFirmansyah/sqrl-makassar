de088ff82446ade1b352a6163e3e2fc9fead0f44
Juan Firmansyah <juanfirmansyah.jf@gmail.com>
2026-09-16 05:48:29 +0800

    fix: done progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 // src/pages/_app.tsx
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Bebas_Neue, Manrope } from "next/font/google";
 import "./globals.css";
 
@@ -19,6 +20,34 @@ const bodyFont = Manrope({
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={`${display.variable} ${bodyFont.variable} font-body`}>
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+        {/* Apple Touch Icon (iOS) */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+
+        {/* PWA / Android */}
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#06070B" />
+      </Head>
+
       <Component {...pageProps} />
     </div>
   );
